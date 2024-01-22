12680224373eea2f5be3e35afd5e75e260518c7a
Sultanalhail <148471227+Sultanalhail@users.noreply.github.com>
2024-01-22 15:36:57 +0000

    Update Lab01_Java.java

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -4,9 +4,9 @@ void testMethod1()
   {
   //First command x = y + z
   //Second commnad print(x)
+// Edit made by Sultan
 
-
-  //Last line print('Good bye')
+  //Last line print('Bye')
 }
 
 void testMethod2()
